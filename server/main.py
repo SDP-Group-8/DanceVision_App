d9322d858d736c66364e4105f378bac60cb9866c
AlexWebb03 <alexanderwebb03@gmail.com>
2024-01-31 13:02:35 +0000

    remove unnecessary files2

--- server/main.py
+++ server/main.py
@@ -1,5 +1,4 @@
 import os
-from pathlib import Path
 import subprocess
 from fastapi import FastAPI, File, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
@@ -32,14 +31,14 @@ async def upload_video(video: UploadFile = File(...)):
     if file_size > (300 * pow(1024, 2)):  # 300 MB
         return {"message": "File size too large"}
 
-    destination_path = "uploads/" + video.filename
+    destination_path = os.path.join("uploads", video.filename)
     os.makedirs(os.path.dirname(destination_path), exist_ok=True)
 
     with open(destination_path, "wb") as f:
         f.write(file_data)
 
     # generate thumbnail
-    img_output_path = "uploads/thumbnails/" + video.filename[0:-4] + ".jpg"
+    img_output_path = os.path.join("uploads", "thumbnails", video.filename[0:-4], ".jpg")
     subprocess.call(['ffmpeg', '-i', destination_path, '-ss', '00:00:00.000', '-vframes', '1', img_output_path])
 
     return {"message": "File uploaded successfully"} 
@@ -47,10 +46,6 @@ async def upload_video(video: UploadFile = File(...)):
 # Retrieve thumbnail icons.
 @app.get("/thumbnails")
 async def get_thumbnails():
-    thumbnail_dir = Path("uploads/thumbnails")
-    thumbnails = []
-    for thumbnail_file in thumbnail_dir.iterdir():
-        if thumbnail_file.is_file():
-            thumbnail_url = url + "/" + f"{thumbnail_file.name}"
-            thumbnails.append(thumbnail_url)
+    thumbnail_dir = os.path.join("uploads", "thumbnails")
+    thumbnails = [os.path.join(url, f"{thumbnail_file.name}") for thumbnail_file in thumbnail_dir.iterdir() if thumbnail_file.is_file()]
     return {"thumbnails": thumbnails}
