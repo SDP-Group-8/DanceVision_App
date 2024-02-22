29a0feeb8c849f6cfd12a2f6443bac6e9e2c8647
AlexWebb03 <alexanderwebb03@gmail.com>
2024-02-22 21:08:22 +0000

    update docstrings

--- server/src/dancevision_server/peer_connection.py
+++ server/src/dancevision_server/peer_connection.py
@@ -9,20 +9,43 @@ from dancevision_server.host_identifiers import RASPBERRY_PI_IDENTIFIER, SERVER_
 
 class PeerConnnection:
     @staticmethod
-    def __get_url(address: str, port: int):
+    def __get_url(address: str, port: int) -> str:
+        """
+        Get server url based on address and port
+        :param address: server address
+        :param port: server port
+        :return: full address
+        """
         return f"http://{address}:{port}"
 
     @staticmethod
-    def __post_session_description(endpoint: str, pc: RTCPeerConnection, host_id: str, address: str, port: int):
+    def __post_session_description(endpoint: str, desc: RTCSessionDescription, host_id: str, address: str, port: int):
+        """
+        Send a post request to a server including a session description as payload
+        :param endpoint: server endpoint
+        :param desc: the session description
+        :param host_id: the id of the video source (to distinguish different connections)
+        :param address: server address
+        :param port: server port
+        :return: response of the post request
+        """
         payload = {
-            "sdp": pc.localDescription.sdp,
-            "type": pc.localDescription.type,
+            "sdp": desc.sdp,
+            "type": desc.type,
             "host_id": host_id
         }
         return requests.post(f"{PeerConnnection.__get_url(address, port)}/{endpoint}", data = json.dumps(payload))
 
     @staticmethod
     async def __get_session_description(endpoint: str, host_id: str, address: str, port: int):
+        """
+        Send a get request to a server to retrieve a session description
+        :param endpoint: server endpoint
+        :param host_id: the id of the video source (to distinguish different connections)
+        :param address: server address
+        :param port: server port
+        :return: response of the get request
+        """
         status_code = status.HTTP_409_CONFLICT
         while status_code != status.HTTP_200_OK:
             url = f"{PeerConnnection.__get_url(address, port)}/{endpoint}?" + urllib.parse.urlencode({"host_id": host_id})
@@ -33,17 +56,42 @@ class PeerConnnection:
 
     @staticmethod
     async def negotiate_receiver(pc: RTCPeerConnection, address: str, port: int, host_id: str = RASPBERRY_PI_IDENTIFIER):
+        """
+        Negotiate a peer connection to receive video
+        :param pc: uninitialized peer connection object
+        :param address: server address
+        :param port: server port
+        :param host_id: the id of the video source (to distinguish different connections)
+        """
         await pc.setLocalDescription(await pc.createOffer())
-        PeerConnnection.__post_session_description("offer", pc, host_id, address, port)
+        PeerConnnection.__post_session_description("offer", pc.localDescription, host_id, address, port)
         connection_answer = await PeerConnnection.__get_session_description("request-answer", host_id, address, port)
         remote_description = RTCSessionDescription(connection_answer["sdp"], connection_answer["type"])
         await pc.setRemoteDescription(remote_description)
 
     @staticmethod
     async def negotiate_sender(pc: RTCPeerConnection, address: str, port: int, host_id: str = SERVER_IDENTIFIER):
+        """
+        Negotiate a peer connection to send video
+        :param pc: uninitialized peer connection object
+        :param address: server address
+        :param port: server port
+        :param host_id: the id of the video source (to distinguish different connections)
+        """
         connection_offer = await PeerConnnection.__get_session_description("request-offer", host_id, address, port)
         offer = RTCSessionDescription(sdp=connection_offer["sdp"], type=connection_offer["type"])
+        PeerConnnection.negotiate_local_sender()
+        PeerConnnection.__post_session_description("answer", pc.localDescription, host_id, address, port)
+
+    @staticmethod
+    async def negotiate_local_sender(pc: RTCPeerConnection, offer: RTCSessionDescription):
+        """
+        Negotiate a peer connection to send video (without querying the server)
+        :param pc: uninitialized peer connection object
+        :param offer: the offer made by the video consumer
+        :return: the answer (session description)
+        """
         await pc.setRemoteDescription(offer)
         answer = await pc.createAnswer()
         await pc.setLocalDescription(answer)
-        PeerConnnection.__post_session_description("answer", pc, host_id, address, port)
+        return answer
