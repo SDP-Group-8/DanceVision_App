258f96b4e09c8b31a164900fbca6cbc36dd07611
wrainf <amirfaiz1806@gmail.com>
2024-03-08 14:40:33 +0000

    feat: add syles for slect video page loading

--- client/src/pages/SelectVideoPage/SelectVideoPage.jsx
+++ client/src/pages/SelectVideoPage/SelectVideoPage.jsx
@@ -3,21 +3,26 @@ import styles from "./SelectVideoPage.module.css";
 
 import VideoDisplay from "../../components/VideoDisplay/VideoDisplay";
 
+const Loader = ({message}) => {
+  return (<div className={styles.SelectVideoPage}><h1 style={{zIndex: 1}}>{message}</h1><div className={styles.gradient}></div></div>)
+}
+
 function SelectVideoPage() {
     const { images, isLoading, error } = useFetchImages(import.meta.env.VITE_API_URL);
 
     console.log(images)
 
     if (isLoading) {
-      return <p>Loading images...</p>;
+      return <Loader message={"Loading Images..."}/>;
     }
   
     if (error) {
-      return <p>Error fetching images: {error.message}</p>;
+      return <Loader message={`Error fetching images: ${error.message}`}/>;
     }
 
     return (
       <div className={styles.SelectVideoPage}>
+        
         <h1 className={styles.header}>Select A Dance Video</h1>
         <div className={styles["thumbnail-container"]}>
           {images.map((image) => 
